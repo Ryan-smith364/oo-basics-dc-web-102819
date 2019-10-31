65f225e54747195b5e0efe846db277befaf11134
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-31 13:56:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
-
+class Book
+  attr_accessor  :page_count, :genre
+  def initialize (name)
+    @name = name
+   
+  end
+  
+  def title(new_title)
+    @title = new_title
+    return @title
+  end
+  
+  def author_name(author_name)
+    @author_name = author_name
+  end
+  
+  
+end
 
